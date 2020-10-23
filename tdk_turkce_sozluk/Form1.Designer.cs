a589fa046a4010f9606c152e17394c7e17cfb5d7
mustafabukulmez <mustafabukulmez@hotmail.com.tr>
2020-10-23 22:16:35 +0300

    HTML Agility ack ile TDK Sitesinden Türkçe kelimelerin anlamlarını bulma uygulaması

---
+++ tdk_turkce_sozluk/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace tdk_turkce_sozluk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ara = new System.Windows.Forms.TextBox();
+            this.btn_ara = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aranacak Kelime";
+            // 
+            // txt_ara
+            // 
+            this.txt_ara.Location = new System.Drawing.Point(117, 24);
+            this.txt_ara.Name = "txt_ara";
+            this.txt_ara.Size = new System.Drawing.Size(154, 20);
+            this.txt_ara.TabIndex = 2;
+            // 
+            // btn_ara
+            // 
+            this.btn_ara.Location = new System.Drawing.Point(277, 24);
+            this.btn_ara.Name = "btn_ara";
+            this.btn_ara.Size = new System.Drawing.Size(75, 23);
+            this.btn_ara.TabIndex = 3;
+            this.btn_ara.Text = "Ara";
+            this.btn_ara.UseVisualStyleBackColor = true;
+            this.btn_ara.Click += new System.EventHandler(this.btn_ara_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 77);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 373);
+            this.textBox1.TabIndex = 5;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 372);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 78);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btn_ara);
+            this.Controls.Add(this.txt_ara);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TDK Kelime Anlam Bulma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ara;
+        private System.Windows.Forms.Button btn_ara;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ tdk_turkce_sozluk/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+using HtmlAgilityPack;
+
+namespace tdk_turkce_sozluk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var appName = Process.GetCurrentProcess().ProcessName + ".exe";
+            SetIE8KeyforWebBrowserControl(appName);
+            webBrowser1.ScriptErrorsSuppressed = true;
+            string url = "https://sozluk.gov.tr/";
+            this.webBrowser1.Navigate(url);
+            await PageLoad(1);
+        }
+
+        private async void btn_ara_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.GetElementById("tdk-srch-input").SetAttribute("value", txt_ara.Text);
+            webBrowser1.Document.GetElementById("tdk-search-btn").InvokeMember("Click");
+            await PageLoad(2); // aramanın yüklenmesini bekliyoruz.
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(webBrowser1.DocumentText);
+            HtmlElement html_maddeler = webBrowser1.Document.GetElementById("maddeler0");
+            try
+            {
+                textBox1.Text = (html_maddeler.InnerText).Replace(@"GÜNCEL TÜRKÇE SÖZLÜK", "").Replace("Birleşik Kelimeler", "");
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("TDK'da böyle bir kelime yer almamaktadır.", "Kelime Yok!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private async Task PageLoad(int TimeOut) // sayfayı bekletme
+        {
+            TaskCompletionSource<bool> PageLoaded = null;
+            PageLoaded = new TaskCompletionSource<bool>();
+            int TimeElapsed = 0;
+            webBrowser1.DocumentCompleted += (s, e) =>
+            {
+                if (webBrowser1.ReadyState != WebBrowserReadyState.Complete) return;
+                if (PageLoaded.Task.IsCompleted) return; PageLoaded.SetResult(true);
+            };
+            while (PageLoaded.Task.Status != TaskStatus.RanToCompletion)
+            {
+                await Task.Delay(10);
+                TimeElapsed++;
+                if (TimeElapsed >= TimeOut * 100) PageLoaded.TrySetResult(true);
+            }
+        }
+
+
+        private void SetIE8KeyforWebBrowserControl(string appName) //web browser sürümü yükseltiyoruz.
+        {
+            RegistryKey Regkey = null;
+            try
+            {
+                if (Environment.Is64BitOperatingSystem)
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Wow6432Node\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                else
+                    Regkey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                if (Regkey == null)
+                {
+                    return;
+                }
+                string FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+                if (FindAppkey == "8000")
+                {
+                    Regkey.Close();
+                    return;
+                }
+                if (string.IsNullOrEmpty(FindAppkey))
+                    Regkey.SetValue(appName, unchecked((int)0x1F40), RegistryValueKind.DWord);
+                FindAppkey = Convert.ToString(Regkey.GetValue(appName));
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                if (Regkey != null) Regkey.Close();
+            }
+        }
+
+    }
+}
